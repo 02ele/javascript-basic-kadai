fb77388f424666cdad9fe4a4d989b91f99f9f56c
haruka.s <105984955+02ele@users.noreply.github.com>
2023-08-02 18:40:47 +0900

    js課題７章アップロード

---
+++ kadai_007/calc.js
@@ -0,0 +1,20 @@
+// 変数宣言・値代入
+let num = Math.floor(Math.random() * 101);
+
+// 出力確認
+console.log(num);
+
+// ３の倍数だった場合
+if (num % 3 === 0 && num % 5 === 0 && num != 0) {
+  console.log('3と5の倍数です');
+}
+else if (num % 3 === 0 && num != 0) {
+  console.log('3の倍数です');
+}
+else if (num % 5 === 0 && num != 0) {
+  console.log('5の倍数です');
+}
+else {
+  console.log(num);
+}
+
