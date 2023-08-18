8385a64b7e3ccd9df0f7884b9e8c38180192397a
haruka.s <105984955+02ele@users.noreply.github.com>
2023-08-18 17:56:36 +0900

    jQuery課題４章アップロード

---
+++ kadai_004/event.js
@@ -0,0 +1,11 @@
+
+$(window).on('load', () => {
+  // ロードしたらコンソールに表示する
+  console.log('loadイベントが発生しました');
+});
+
+$(window).on('scroll', () => {
+  // スクロールしたらコンソールに表示する
+  console.log('scrollイベントが発生しました');
+});
+
